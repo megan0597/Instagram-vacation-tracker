1a95d5a339fc8bcdc27c528df714f95597d97e5e
Megan <megan0597@hotmail.com>
2016-09-12 09:44:00 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,3 +1,4 @@
 <?php
 define("INSTAGRAM_CLIENT_ID", "Fill in your own client id here");
 define("INSTAGRAM_CLIENT_SECRET", "Fill in your own client secret here");
+define("INSTAGRAM_ACCESS_TOKEN","Fill in your own access token here");
